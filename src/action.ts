584deddc33c4a2af017403d668265cea729412fe
hanseltime <hanseltime@gmail.com>
2024-03-10 22:04:41 -0600

    fix: styling

--- src/action.ts
+++ src/action.ts
@@ -1,7 +1,7 @@
 import * as core from "@actions/core";
 
 interface ActionOptions {
-    // TODO: add your actions options here - must mact in action.yml
+  // TODO: add your actions options here - must mact in action.yml
 }
 
 const options: ActionOptions = {
