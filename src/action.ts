07bd6f5e30fe9af87e63b6ae8f690865225dc269
hanseltime <hanseltime@gmail.com>
2024-11-19 13:45:57 -0700

    style: lint fix

--- src/action.ts
+++ src/action.ts
@@ -1,11 +1,11 @@
-import * as core from "@actions/core";
+// import * as core from "@actions/core";
 
-interface ActionOptions {
-  // TODO: add your actions options here - must mact in action.yml
-}
+// interface ActionOptions {
+// TODO: add your actions options here - must mact in action.yml
+// }
 
-const options: ActionOptions = {
-  // repoPath: core.getInput("repoPath"),
-};
+// const options: ActionOptions = {
+// repoPath: core.getInput("repoPath"),
+// };
 
 // TODO: your action function call with options
